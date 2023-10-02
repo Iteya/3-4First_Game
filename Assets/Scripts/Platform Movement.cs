0d6e1dd9533ae856189c0e9a4807a757d2981b75
Iteya <jandrico7658@student.pps.net>
2023-10-02 11:40:58 -0700

    Added Coin Collection

--- Assets/Scripts/Platform Movement.cs
+++ Assets/Scripts/Platform Movement.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -10,7 +11,9 @@ public class PlatformMovement : MonoBehaviour
     public bool inCaves;
     public int ySpeed;
     public int xSpeed;
-    // Update is called once per frame
+    public int coins;
+    public TextMeshProUGUI coinsCollected;
+    public TextMeshProUGUI healthLeft;
     void Update()
     {
         if (inCaves)
@@ -18,13 +21,15 @@ public class PlatformMovement : MonoBehaviour
             ySpeed = 0;
         }
         transform.Translate((Input.GetAxis("Horizontal")* xSpeed )*Time.deltaTime, (Input.GetAxis("Vertical") * ySpeed) * Time.deltaTime, 0);
+        Debug.Log("coins: " + coins);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Coin")
+        if (other.CompareTag("Coin"))
         {
-            
-        }
+            coins += 1;
+            other.gameObject.SetActive(false);
+        } 
     }
 }
